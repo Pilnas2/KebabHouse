af3acc6fbc1155dfb3c4657c80a3d4035eb12b98
Martin Pilný <martin.pilnas@email.cz>
2024-08-29 14:04:24 +0200

    edit

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-function orderNow() {
-    alert('Objednávka byla přijata! Zavolejte nám na 332 999 999 nebo objednejte přes web.');
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+    
+    function changeNav() {
+      let index = sections.length;
+  
+      while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+      navLinks.forEach((link) => link.classList.remove('active'));
+      navLinks[index].classList.add('active');
+    }
+  
+    changeNav();
+    window.addEventListener('scroll', changeNav);
+  });
+
+  // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
+  
